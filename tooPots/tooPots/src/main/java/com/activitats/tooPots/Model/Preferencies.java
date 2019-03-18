1da5e58cb555f5238688b928d290f150a6433d9f
Carles Torrat <al341805@uji.es>
2019-03-18 15:01:37 +0100

    Modelo, Dao i Mapper
    Nivell, Preferencies i Imatges Promocionals. Corresponents models, daos i mappers.

---
+++ tooPots/tooPots/src/main/java/com/activitats/tooPots/Model/Preferencies.java
@@ -0,0 +1,25 @@
+package com.activitats.tooPots;
+
+public class Preferencies{
+    private short preferenciaId;
+    private String nomTipusActivitat;
+
+    public Preferencies() {
+    }
+
+    public short getPreferenciaId() {
+        return preferenciaId;
+    }
+
+    public void setPreferenciaId(short preferenciaId) {
+        this.preferenciaId = preferenciaId;
+    }
+
+    public String getNomTipusActivitat() {
+        return nomTipusActivitat;
+    }
+
+    public void setNomTipusActivitat(String nomTipusActivitat) {
+        this.nomTipusActivitat = nomTipusActivitat;
+    }
+}
